87aefe4a7ba0f4bb5d798601c07e8a024096b2c3
hvgazula <hvgazula@umich.edu>
2021-02-23 02:21:02 -0500

    probably working code

--- code/tfsenc_main.py
+++ code/tfsenc_main.py
@@ -1,15 +1,35 @@
 import argparse
+import csv
 import glob
 import os
 import pickle
 from datetime import datetime
+from functools import partial
+from multiprocessing import Pool
 
 import numpy as np
 import pandas as pd
 from scipy.io import loadmat
 from tfsenc_pca import run_pca
+from tfsenc_phase_shuffle import phase_randomize_1d
 from tfsenc_read_datum import read_datum
-from tfsenc_utils import encoding_regression, load_header, setup_environ
+from tfsenc_utils import (append_jobid_to_string, create_output_directory,
+                          encoding_regression, encoding_regression_pr,
+                          load_header, setup_environ)
+
+
+def main_timer(func):
+    def function_wrapper():
+        start_time = datetime.now()
+        print(f'Start Time: {start_time.strftime("%A %m/%d/%Y %H:%M:%S")}')
+
+        func()
+
+        end_time = datetime.now()
+        print(f'End Time: {end_time.strftime("%A %m/%d/%Y %H:%M:%S")}')
+        print(f'Total runtime: {end_time - start_time} (HH:MM:SS)')
+
+    return
 
 
 def load_pickle(file):
@@ -50,6 +70,7 @@ def parse_arguments():
     parser.add_argument('--electrodes', nargs='*', type=int)
     parser.add_argument('--npermutations', type=int, default=1)
     parser.add_argument('--min-word-freq', nargs='?', type=int, default=1)
+    parser.add_argument('--job-id', type=int, default=0)
 
     group = parser.add_mutually_exclusive_group()
     group.add_argument('--sid', nargs='?', type=int, default=None)
@@ -97,7 +118,7 @@ def load_electrode_data(args, elec_id):
     convos = sorted(glob.glob(os.path.join(DATA_DIR, str(args.sid), '*')))
 
     all_signal = []
-    for convo in convos:
+    for convo in convos[:1]:
         file = glob.glob(
             os.path.join(convo, 'preprocessed',
                          '*' + str(elec_id) + '.mat'))[0]
@@ -153,11 +174,11 @@ def load_processed_datum(args):
     return concatenated_datum
 
 
-def process_subjects(args, datum):
+def process_subjects(args):
     """Run encoding on particular subject (requires specifying electrodes)
     """
     electrode_info = load_pickle(
-        os.path.join(args.PICKLE_DIR, str(args.sid), args.electrode_file))
+        os.path.join(args.PICKLE_DIR, args.electrode_file))
 
     # trimmed_signal = trimmed_signal_dict['trimmed_signal']
 
@@ -173,21 +194,21 @@ def process_subjects(args, datum):
             for key in args.electrodes
         }
 
-    # Loop over each electrode
-    for elec_id, elec_name in electrode_info.items():
+    # # Loop over each electrode
+    # for elec_id, elec_name in electrode_info.items():
 
-        if elec_name is None:
-            print(f'Electrode ID {elec_id} does not exist')
-            continue
+    #     if elec_name is None:
+    #         print(f'Electrode ID {elec_id} does not exist')
+    #         continue
 
-        elec_signal = load_electrode_data(args, elec_id)
-        # datum = load_processed_datum(args)
+    #     elec_signal = load_electrode_data(args, elec_id)
+    #     # datum = load_processed_datum(args)
 
-        encoding_regression(args, args.sid, datum, elec_signal, elec_name)
+    #     encoding_regression(args, datum, elec_signal, elec_name)
 
-    # write_electrodes(args, electrode_names)
+    # # write_electrodes(args, electrode_names)
 
-    return
+    return electrode_info
 
 
 def process_sig_electrodes(args, datum):
@@ -228,10 +249,59 @@ def process_sig_electrodes(args, datum):
     return
 
 
-if __name__ == "__main__":
-    start_time = datetime.now()
-    print(f'Start Time: {start_time.strftime("%A %m/%d/%Y %H:%M:%S")}')
+def dumdum1(iter_idx, args, datum, signal, name):
+    np.random.seed(iter_idx)
+    new_signal = phase_randomize_1d(signal)
+    (prod_corr, comp_corr) = encoding_regression_pr(args, datum, new_signal,
+                                                    name)
 
+    return (prod_corr, comp_corr)
+
+
+def write_output(args, output_mat, name, output_str):
+
+    output_dir = create_output_directory(args)
+
+    if all(output_mat):
+        trial_str = append_jobid_to_string(args, output_str)
+        filename = os.path.join(output_dir, name + trial_str + '.csv')
+        with open(filename, 'w') as csvfile:
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerows(output_mat)
+
+
+def this_is_where_you_perform_regression(args, electrode_info, datum):
+
+    # Loop over each electrode
+    for elec_id, elec_name in electrode_info.items():
+
+        if elec_name is None:
+            print(f'Electrode ID {elec_id} does not exist')
+            continue
+
+        elec_signal = load_electrode_data(args, elec_id)
+
+        # Perform encoding/regression
+        if args.phase_shuffle:
+            with Pool(16) as pool:
+                corr = pool.map(
+                    partial(dumdum1,
+                            args=args,
+                            datum=datum,
+                            signal=elec_signal,
+                            name=elec_name), range(args.npermutations))
+
+            prod_corr, comp_corr = map(list, zip(*corr))
+            print("i am out")
+            write_output(args, comp_corr, elec_name, 'prod')
+            write_output(args, comp_corr, elec_name, 'comp')
+        else:
+            encoding_regression(args, datum, elec_signal, elec_name)
+    return
+
+
+@main_timer
+def main():
     # Read command line arguments
     args = parse_arguments()
 
@@ -245,12 +315,11 @@ if __name__ == "__main__":
         datum = run_pca(args, datum)
 
     # Processing significant electrodes or individual subjects
-    if args.sig_elec_file:
-        process_sig_electrodes(args, datum)
-    else:
-        process_subjects(args, datum)
+    electrode_info = process_subjects(args)
+    this_is_where_you_perform_regression(args, electrode_info, datum)
 
-    end_time = datetime.now()
-    print(f'End Time: {end_time.strftime("%A %m/%d/%Y %H:%M:%S")}')
+    return
 
-    print(f'Total runtime: {end_time - start_time} (HH:MM:SS)')
+
+if __name__ == "__main__":
+    main()
--- code/tfsenc_phase_shuffle.py
+++ code/tfsenc_phase_shuffle.py
@@ -1,28 +1,29 @@
 import math
 
 import numexpr as ne
+# import jax.numpy as jnp
 import numpy as np
 from scipy.fftpack import fft, ifft
 
 
-def phase_shuffle(input_signal, signal_dim):
+def phase_shuffle(input_signal):
     # Returns a vector of the same size and amplitude spectrum but with shuffled
     # phase information.
 
-    N = input_signal.shape[signal_dim]
+    N = input_signal.shape[1]
 
     if N % 2:
         h = input_signal[-1]
         input_signal = input_signal[:-1]
 
-    F = abs(np.fft.fft(input_signal))
+    F = np.fft.fft(input_signal, axis=-1)
     t = np.zeros(F.shape)
-    t[:, 1:math.
+    t[0, 1:math.
       floor(N /
             2)] = np.random.rand(math.floor(N / 2) - 1) * 2 * math.pi - math.pi
-    t[:, (math.floor(N / 2) + 1)] = -t[math.floor(N / 2) - 1:0:-1]
+    t[:, (math.floor(N / 2) + 1):] = -t[:, math.floor(N / 2) - 1:0:-1]
 
-    output_signal = abs(np.fft.ifft(F * np.exp(1j * t)))
+    output_signal = np.real(np.fft.ifft(F * np.exp(1j * t), axis=-1))
 
     if N % 2:
         output_signal.append(h)
@@ -38,8 +39,6 @@ def phase_randomize(data):
 
     n_examples, n_lags, n_samples = data.shape
 
-    np.random.seed(seed=np.random.randint(10000))
-
     # Get randomized phase shifts
     if n_samples % 2 == 0:
         pos_freq = np.arange(1, n_samples // 2)
@@ -48,21 +47,21 @@ def phase_randomize(data):
         pos_freq = np.arange(1, (n_samples - 1) // 2 + 1)
         neg_freq = np.arange(n_samples - 1, (n_samples - 1) // 2, -1)
 
-    phase_shifts = (np.random.rand(n_examples, n_lags, len(pos_freq)) * 2 *
-                    np.math.pi)
+    phase_shifts = np.random.rand(n_examples, n_lags,
+                                  len(pos_freq)) * 2 * np.math.pi
 
     # Fast Fourier transform along time dimension of data
     fft_data = np.fft.fft(data, axis=-1)
 
     # Shift pos and neg frequencies symmetrically, to keep signal real
-    # fft_data[:, :, pos_freq] *= jnp.exp(1j * phase_shifts)
-    # fft_data[:, :, neg_freq] *= jnp.exp(-1j * phase_shifts)
+    fft_data[:, :, pos_freq] *= np.exp(1j * phase_shifts)
+    fft_data[:, :, neg_freq] *= np.exp(-1j * phase_shifts)
 
-    a = fft_data[:, :, pos_freq]
-    b = fft_data[:, :, neg_freq]
+    # a = fft_data[:, :, pos_freq]
+    # b = fft_data[:, :, neg_freq]
 
-    fft_data[:, :, pos_freq] = ne.evaluate('a * exp(1j * phase_shifts)')
-    fft_data[:, :, neg_freq] = ne.evaluate('b * exp(-1j * phase_shifts)')
+    # fft_data[:, :, pos_freq] = ne.evaluate('a * exp(1j * phase_shifts)')
+    # fft_data[:, :, neg_freq] = ne.evaluate('b * exp(-1j * phase_shifts)')
 
     # Inverse FFT to put data back in time domain
     shifted_data = np.real(ifft(fft_data, axis=-1))
@@ -79,23 +78,23 @@ def phase_randomize_1d(data):
 
     # Get randomized phase shifts
     if n_samples % 2 == 0:
-        pos_freq = np.arange(1, data.shape[0] // 2)
-        neg_freq = np.arange(data.shape[0] - 1, data.shape[0] // 2, -1)
+        pos_freq = np.arange(1, n_samples // 2)
+        neg_freq = np.arange(data.shape[0] - 1, n_samples // 2, -1)
     else:
         pos_freq = np.arange(1, (data.shape[0] - 1) // 2 + 1)
-        neg_freq = np.arange(data.shape[0] - 1, (data.shape[0] - 1) // 2, -1)
+        neg_freq = np.arange(data.shape[0] - 1, (n_samples - 1) // 2, -1)
 
     phase_shifts = np.random.rand(len(pos_freq), 1) * 2 * np.math.pi
 
     # Fast Fourier transform along time dimension of data
-    fft_data = fft(data, axis=-1)
+    fft_data = fft(data, axis=0)
 
     # Shift pos and neg frequencies symmetrically, to keep signal real
     fft_data[pos_freq, :] *= np.exp(1j * phase_shifts)
     fft_data[neg_freq, :] *= np.exp(-1j * phase_shifts)
 
     # Inverse FFT to put data back in time domain
-    shifted_data = np.real(ifft(fft_data, axis=-1))
+    shifted_data = np.real(ifft(fft_data, axis=0))
 
     # Make it a contiguous array (for numba compatibility])
     shifted_data = np.ascontiguousarray(shifted_data)
@@ -104,6 +103,27 @@ def phase_randomize_1d(data):
 
 
 if __name__ == '__main__':
-    input_vec = np.random.rand(10, 1)
-    output_vec0 = phase_randomize_1d(input_vec)
-    print(output_vec0.shape)
+    iter = 10
+
+    input_vec = np.arange(1, 101).reshape(1, -1)
+    print(f'input vec sum: {sum(sum(input_vec))}')
+    for i in range(iter):
+        np.random.seed(i)
+        output_vec0 = phase_shuffle(input_vec)
+        print(sum(sum(output_vec0)))
+
+    print("============================")
+    input_vec = np.arange(1, 101).reshape(-1, 1)
+    print(f'input vec sum: {sum(sum(input_vec))}')
+    for i in range(iter):
+        np.random.seed(i)
+        output_vec1 = phase_randomize_1d(input_vec)
+        print(sum(sum(output_vec1)))
+
+    print("============================")
+    input_vec = np.arange(1, 101).reshape(1, 1, -1)
+    print(f'input vec sum: {sum(sum(sum((input_vec))))}')
+    for i in range(iter):
+        np.random.seed(i)
+        output_vec2 = phase_randomize(input_vec)
+        print(sum(sum(sum(output_vec2))))
