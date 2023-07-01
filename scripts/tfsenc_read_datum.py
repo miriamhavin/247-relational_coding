6cfdaa149630055ff794bb8b5edb0ecf6d2ea87d
Ken Wang <kw1166@della-gpu.princeton.edu>
2023-07-01 11:32:36 -0400

    delete plotting

--- scripts/tfsenc_read_datum.py
+++ scripts/tfsenc_read_datum.py
@@ -16,8 +16,8 @@ def remove_punctuation(df):
 
 def drop_nan_embeddings(df):
     """Drop rows containing all nan's for embedding"""
-    df["is_nan"] = df["embeddings"].apply(lambda x: np.isnan(x).all())
-    df = df[~df["is_nan"]]
+    is_nan = df["embeddings"].apply(lambda x: np.isnan(x).all())
+    df = df[~is_nan]
 
     return df
 
