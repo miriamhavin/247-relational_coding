2ea08943df78c4be59cd83445607210882fb6399
Ken Wang <kw1166@della-gpu.princeton.edu>
2023-07-02 18:57:01 -0400

    fix bug

--- scripts/tfsenc_main.py
+++ scripts/tfsenc_main.py
@@ -17,6 +17,7 @@ from tfsenc_utils import (
     run_regression,
     write_encoding_results,
 )
+from collections import Counter
 from utils import load_pickle, main_timer, write_config
 
 
@@ -41,40 +42,30 @@ def return_stitch_index(args):
 
 
 def skip_elecs_done(args, electrode_info):
-    # fine elecs done
+    # find elecs done
     elecs_done = [
         os.path.basename(file)
         for file in glob.glob(os.path.join(args.full_output_dir, "*_*.csv"))
     ]
-    elecs_done = sorted(elecs_done)
+    elecs_done = [
+        elec.replace("_fold", "")
+        .replace("prod", "comp")
+        .replace("_comp.csv", "")
+        for elec in elecs_done
+    ]
+    elecs_counts = Counter(elecs_done)
+
     elecs_num = len(electrode_info)
-
-    while len(elecs_done) > 0:
-
-        elec_done = elecs_done[0]
-        skip = False
-
-        # check if elec is actually done
-        if "comp" in elec_done:  # has comp
-            if args.project_id == "podcast":  # podcast
-                skip = True
-            elif (
-                elecs_done[1].replace("prod", "comp") == elec_done
-            ):  # tfs, has prod
-                skip = True
-                elecs_done.pop(1)
-        elecs_done.pop(0)
-
-        if skip:  # skip elec
-            elec_done = elec_done.replace("_comp.csv", "")
-            print(f"Skipping elec {elec_done}")
-            sid_string = elec_done[: elec_done.find("_")]
+    for elec, count in elecs_counts.most_common():
+        if count == 4:
+            print(f"Skipping elec {elec}")
+            sid_string = elec[: elec.find("_")]
             if sid_string.isdigit():  # actually a sid
-                elec_done = elec_done.replace(f"{sid_string}_", "")
+                elec = elec.replace(f"{sid_string}_", "")
             electrode_info = {
                 key: val
                 for key, val in electrode_info.items()
-                if (val != elec_done or key[0] != int(sid_string))
+                if (val != elec or key[0] != int(sid_string))
             }
             elecs_num -= 1
 
@@ -107,7 +98,9 @@ def process_subjects(args):
         )
         assert len(sig_elec_list) == len(sid_sig_elec_list), "Sig Elecs Missing"
         electrode_info = {
-            (values["subject"], values["electrode_id"]): values["electrode_name"]
+            (values["subject"], values["electrode_id"]): values[
+                "electrode_name"
+            ]
             for _, values in sid_sig_elec_list.iterrows()
         }
 
@@ -117,7 +110,8 @@ def process_subjects(args):
             (args.sid, key): next(
                 iter(
                     df.loc[
-                        (df.subject == str(args.sid)) & (df.electrode_id == key),
+                        (df.subject == str(args.sid))
+                        & (df.electrode_id == key),
                         "electrode_name",
                     ]
                 ),
@@ -179,7 +173,9 @@ def single_electrode_encoding(electrode, args, datum, stitch_index):
         fold_cat_prod = []
         fold_cat_comp = get_kfolds(comp_X, args.fold_num)
     elif (
-        "single-conv" in args.datum_mod or args.conversation_id or args.sid == 798
+        "single-conv" in args.datum_mod
+        or args.conversation_id
+        or args.sid == 798
     ):  # 1 conv
         fold_cat_prod = get_kfolds(prod_X, args.fold_num)
         fold_cat_comp = get_kfolds(comp_X, args.fold_num)
@@ -187,11 +183,15 @@ def single_electrode_encoding(electrode, args, datum, stitch_index):
         args.project_id == "tfs"
         and elec_datum.conversation_id.nunique() < args.fold_num
     ):  # num of convos less than num of folds (special case for 7170)
-        print(f"{args.sid} {elec_name} has less conversations than the number of folds")
+        print(
+            f"{args.sid} {elec_name} has less conversations than the number of folds"
+        )
         return (args.sid, elec_name, 1, 1)
     else:
         # Get groupkfolds
-        fold_cat_prod, fold_cat_comp = get_groupkfolds(elec_datum, X, Y, args.fold_num)
+        fold_cat_prod, fold_cat_comp = get_groupkfolds(
+            elec_datum, X, Y, args.fold_num
+        )
         if (
             len(np.unique(fold_cat_prod)) < args.fold_num
             or len(np.unique(fold_cat_comp)) < args.fold_num
@@ -238,14 +238,16 @@ def parallel_encoding(args, electrode_info, datum, stitch_index, parallel=True):
     #     parallel = False
     if parallel:
         print("Running all electrodes in parallel")
-        summary_file = os.path.join(args.full_output_dir, "summary.csv")  # summary file
+        summary_file = os.path.join(
+            args.full_output_dir, "summary.csv"
+        )  # summary file
         p = Pool(processes=get_cpu_count())  # multiprocessing
-        
+
         # Skipping elecs already done
         if os.path.exists(summary_file):  # previous job
             print("Previously ran the same job, checking for elecs done")
             electrode_info = skip_elecs_done(args, electrode_info)
-        
+
         with open(summary_file, "w") as f:
             writer = csv.writer(f, delimiter=",", lineterminator="\r\n")
             writer.writerow(("sid", "electrode", "prod", "comp"))
@@ -269,7 +271,6 @@ def parallel_encoding(args, electrode_info, datum, stitch_index, parallel=True):
 
 @main_timer
 def main():
-
     # Read command line arguments
     args = parse_arguments()
 
