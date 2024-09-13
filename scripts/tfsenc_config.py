b8abe8bec2d50a1a029474edfe0038651d37ae04
cchen23 <cathychen2357@gmail.com>
2024-09-13 18:30:31 -0400

    MAINT only use torch_cuda himalaya backend if cuda is available.

--- scripts/tfsenc_config.py
+++ scripts/tfsenc_config.py
@@ -4,6 +4,7 @@ import argparse
 import getpass
 import subprocess
 import numpy as np
+import torch
 from himalaya.backend import set_backend
 
 ELEC_SIGNAL_PREPROCESS_MAP = {
@@ -146,9 +147,12 @@ def setup_environ(args):
     args.output_dir = os.path.join(OUTPUT_DIR, RESULT_PARENT_DIR, RESULT_CHILD_DIR)
     os.makedirs(args.output_dir, exist_ok=True)
 
-    if args.ridge:
+    if torch.cuda.is_available():
         print("set backend to cuda")
         backend = set_backend("torch_cuda", on_error="warn")
+    else:
+        print("set backend to cpu numpy")
+        backend = set_backend("numpy", on_error="warn")
 
     os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:512"  # HACK
 
